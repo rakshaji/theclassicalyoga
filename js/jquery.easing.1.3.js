c3f0325f87f040640ac1c7770c5d2c3ee4e09d91
rakshaji <rakshasinghania@gmail.com>
2021-02-28 09:22:32 +0530

    added certificate logo and small changes

--- js/jquery.easing.1.3.js
+++ js/jquery.easing.1.3.js
@@ -9,7 +9,7 @@
  * Open source under the BSD License. 
  * 
  * Copyright Ã‚Â© 2008 George McGinley Smith
- * All rights reserved.
+ * All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> .
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
@@ -178,7 +178,7 @@ jQuery.extend( jQuery.easing,
  * Open source under the BSD License. 
  * 
  * Copyright Ã‚Â© 2001 Robert Penner
- * All rights reserved.
+ * All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> .
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
